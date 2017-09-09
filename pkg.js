da5ec6b91b8eaf8d67fcadfa78fd5ad97cbebab3
hongtuan <tht@sina.com>
2017-09-09 22:36:53 +0800

    rebuild package module

--- pkg.js
+++ pkg.js
@@ -2,58 +2,52 @@ var fs = require('fs');
 var path = require('path');
 var archiver = require('archiver');
 var moment = require('moment');
-
-var archive = archiver('zip');
-var archive = archiver('zip', {
-  comment:'comment test.',
-  forceLocalTime:true, //forceLocalTime
-  forceUTC: false,
-  zlib: { level: 9 }   // Sets the compression level.
-});
-
 process.env.TZ = 'Asia/Shanghai';
 
-archive.on('error', function(err){
-  throw err;
-});
-
-var pkgFileName = `../packages/pkg_${moment().format('YYYYMMDDHHmm')}.zip`;
-var fileObj = path.join(__dirname,pkgFileName);
-fs.exists(fileObj, function(exists) {
-  if(exists) {
-    //Show in green
-    console.log('File exists. Deleting now ...');
-    fs.unlink(fileObj, function(err) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("File ["+fileObj+"] deleted successfully!");
-    });
-  } else {
-    //Show in red
-    console.log('File not found, so just create one.');
+function rmFile(_file){
+  if(fs.existsSync(_file) && fs.statSync(_file).isFile()){
+    fs.unlinkSync(_file);
+    console.log(_file,'delete over.');
   }
-});
+}
 
+function doUpdateTask(updateFileName){
+  console.log('update file to remote host...');
+  var cp = require('child_process');
+  var usPath = path.join(__dirname,'../packages/ruc.cmd');
+  var usWorkPath = path.join(__dirname,'../packages/');
+  cp.execFile(usPath,[usWorkPath,updateFileName],
+    function(err,stdout,stderr){
+      if(err) console.log(err);
+      if(stdout) console.log('stdout',stdout);
+      if(stderr) console.log('stderr',stderr);
+      console.log(`file ${updateFileName} update to remote host over.`);
+    }
+  );
+}
+
+//构建打包文件名
+var pkgFileName = `pkg_${moment().format('YYYYMMDDHHmm')}.zip`;
+//打包文件全路径
+var pkgFile = path.join(__dirname,`../packages/${pkgFileName}`);
+//删除已存在的文件
+rmFile(pkgFile);
 console.log('Prepare to do zip task...');
-//delay 2 seconds,then do zip task.
-setTimeout(function() {
-  console.log('zip task begin.');
-  var output = fs.createWriteStream(pkgFileName);
-  archive.pipe(output);
-  // listen for all archive data to be written
-  output.on('close', function() {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-    console.log('pkg over.',pkgFileName);
-    console.log('sft',pkgFileName);
+console.log('zip task begin.');
+
+function createSrcPkg(cb){
+  var archive = archiver('zip', {
+    comment:'src package.',
+    forceLocalTime:true, //forceLocalTime
+    forceUTC: false,
+    zlib: { level: 9 }   // Sets the compression level.
   });
 
   //append version info
   archive.append(
     `build time:${moment().format('YYYY-MM-DD HH:mm:ss')}`,
     { name: 'version.txt' });
-
+  //添加特定的文件和目录
   archive.file('.env',{name:'.env'});
   archive.file('expserver.js',{name:'expserver.js'});
   archive.file('package4server.json',{name:'package.json'});
@@ -67,6 +61,23 @@ setTimeout(function() {
   archive.directory('app_api/');
   archive.directory('app_server/');
   archive.finalize();
-  //callback(null);
-}, 2000);
+
+  archive.on('error', function(err){
+    throw err;
+  });
+
+  var output = fs.createWriteStream(pkgFile);
+  archive.pipe(output);
+
+  // listen for all archive data to be written
+  output.on('close', function() {
+    console.log('file',pkgFileName,'create over.',Math.ceil(archive.pointer()/1024) + ' K');
+    if(cb) cb();
+  });
+}
+
+createSrcPkg(() => {
+  doUpdateTask(pkgFileName);
+  //console.log(pkgFileName);
+});
 
